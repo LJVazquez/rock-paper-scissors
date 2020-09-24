43b7c36c587f34c50d9f3e97cc887a8e500a1a20
Ljvazquez <ljvazquez00@gmail.com>
2020-09-24 19:46:20 -0300

    finished basic prototype

--- game.js
+++ game.js
@@ -18,6 +18,8 @@ let computerSelection;
 let playerSelection;
 const playerScoreVisual = document.querySelector('#player-score');
 const computerScoreVisual = document.querySelector('#computer-score');
+const results = document.querySelector('#results');
+const reset = document.querySelector('#reset');
 let playerScore = 0;
 let computerScore = 0;
 let computerHand = document.querySelector('#computer-hand');
@@ -51,11 +53,46 @@ function playRound(){
         roundResult.textContent = 'Computer wins!';
         return;
     }
+
+}
+
+function gameResult(){
+    if(playerScore === 5){
+        results.textContent = 'Player wins!'
+        results.classList.add('text-success');
+        rock.removeEventListener('click', playRound);
+        paper.removeEventListener('click', playRound);
+        scissor.removeEventListener('click', playRound);
+    }
+    if(computerScore === 5){
+        results.textContent = 'Computer wins!'
+        results.classList.add('text-danger');
+        rock.removeEventListener('click', playRound);
+        paper.removeEventListener('click', playRound);
+        scissor.removeEventListener('click', playRound);
+    }
+}
+
+function resetGame(){
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreVisual.textContent = playerScore;
+    computerScoreVisual.textContent = computerScore;
+    rock.addEventListener('click', playRound);
+    paper.addEventListener('click', playRound);
+    scissor.addEventListener('click', playRound);
+    results.textContent = 'Best of 5!'
+    results.classList.remove('text-success')
+    results.classList.remove('text-danger')
 }
 
 rock.addEventListener('click', changeSelection);
 rock.addEventListener('click', playRound);
+rock.addEventListener('click', gameResult);
 paper.addEventListener('click', changeSelection);
 paper.addEventListener('click', playRound);
+paper.addEventListener('click', gameResult);
 scissor.addEventListener('click', changeSelection);
 scissor.addEventListener('click', playRound);
+scissor.addEventListener('click', gameResult);
+reset.addEventListener('click', resetGame);
